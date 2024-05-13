86955c7acbc8d4ff5b82d4e896cc06607d8083a6
ErnestoAragon03 <ernesto.aragon@galileo.edu>
2024-05-13 14:02:04 -0600

    Conexión entre PHP y SQLS establecida, Apache funcionando

---
+++ Conexion.php
@@ -0,0 +1,15 @@
+<?php
+$serverName = "localhost"; //serverName\instanceName
+
+// Puesto que no se han especificado UID ni PWD en el array  $connectionInfo,
+// La conexión se intentará utilizando la autenticación Windows.
+$connectionInfo = array( "Database"=>"CCVDB");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
